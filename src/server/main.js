6807bc13fcc7e69869fc8cfe3b3e8217d0641304
Alyssa Carter <trillionalyssa@gmail.com>
2023-01-16 07:10:59 -0500

    Factor out file manipulation from server

--- src/server/main.js
+++ src/server/main.js
@@ -4,44 +4,48 @@ const app = express();
 const port = 3000;
 
 async function handleTokensGet(req, resp) {
-    const data = await fs.open('./data/tabletops.json')
-          .then((filehandle) => {
-              const str = filehandle.readFile();
-              filehandle.close();
-              return str;
-          })
-          .then(JSON.parse);
-    resp.send(data);
+  resp.send(await loadTokens());
 }
 async function handleTokensPost(req, resp) {
-    const file = fs.open('./data/tabletops.json')
-          .then((filehandle) => {
-              const str = filehandle.readFile();
-              filehandle.close();
-              return str;
-          })
-          .then(JSON.parse);
-    const tokens = req.body;
-    const tabletop = await file;
-    for (const token of tokens) {
-        let found = false;
-        for (const [i, existing] of tabletop.entries()) {
-            if (existing.id == token.id) {
-                tabletop[i] = token;
-                found = true;
-                break;
-            }
-        }
-        if (!found) {
-            tabletop.push(token);
-        }
+  const tokens = req.body;
+  const tabletop = await loadTokens();
+  for (const token of tokens) {
+    let found = false;
+    for (const [i, existing] of tabletop.entries()) {
+      if (existing.id == token.id) {
+        tabletop[i] = token;
+        found = true;
+        break;
+      }
     }
-    fs.open('./data/tabletops.json', 'w')
-        .then((filehandle) => {
-            filehandle.writeFile(JSON.stringify(tabletop));
-            filehandle.close();
-        });
-    resp.send();
+    if (!found) {
+      tabletop.push(token);
+    }
+  }
+  writeTokens(tabletop);
+  resp.send();
+}
+
+async function handleTokensDelete(req, resp) {
+
+}
+
+function loadTokens() {
+  return fs.open('./data/tabletops.json')
+           .then((filehandle) => {
+             const str = filehandle.readFile();
+             filehandle.close();
+             return str;
+           })
+           .then(JSON.parse);
+}
+
+function writeTokens(tokens) {
+  return fs.open('./data/tabletops.json', 'w')
+           .then((filehandle) => {
+             filehandle.writeFile(JSON.stringify(tokens));
+             filehandle.close();
+           });
 }
 
 app.use(express.static('static'));
@@ -52,5 +56,5 @@ app.get('/api/tokens', handleTokensGet);
 app.post('/api/tokens', handleTokensPost);
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
